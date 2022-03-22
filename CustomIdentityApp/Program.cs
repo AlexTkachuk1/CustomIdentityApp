7b7dc119d1fe07e3a9148c1f4318e1eac9696b26
AlexTkachuk1 <Lyucifer97@list.ru>
2022-03-22 20:30:42 +0300

    Add SeedData

--- CustomIdentityApp/Program.cs
+++ CustomIdentityApp/Program.cs
@@ -26,6 +26,22 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var userManager = services.GetRequiredService<UserManager<User>>();
+        var rolesManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+        await SeedData.InitializeAsync(userManager, rolesManager);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while seeding the database.");
+    }
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
